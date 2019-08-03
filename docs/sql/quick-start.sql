157877b0e0df6282289e9010ceed54f91fd242e9
hylexus <hylexus@163.com>
2019-08-04 00:23:49 +0800

    喝醉了，不想写了，其实我不知道写这些有什么意义V_V

--- docs/sql/quick-start.sql
+++ docs/sql/quick-start.sql
@@ -11,5 +11,8 @@ create table yassos_user
     avatarUrl          varchar(256) not null default ''
 ) comment 'userDetails for yassos';
 
-INSERT INTO yassos_user (name, password, locked, credential_expired, avatarUrl) 
-VALUES ('admin', '1234561', 0, 0, 'https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike92%2C5%2C5%2C92%2C30/sign=4665601df0039245b5b8e95de6fdcfa7/503d269759ee3d6dd050111b4f166d224e4ade48.jpg');
+INSERT INTO yassos_user (name, password, locked, credential_expired, avatarUrl)
+VALUES ('yassos', 'yassos', 0, 0, 'https://avatars1.githubusercontent.com/u/13914832'),
+       ('hylexus', 'yassos', 0, 0, 'https://avatars1.githubusercontent.com/u/13914832'),
+       ('dfEric', 'yassos', 0, 0, 'https://avatars0.githubusercontent.com/u/12884239')
+;
